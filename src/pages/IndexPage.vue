ce42df316608383a21b8b754da7176b378ab9eca
sivagorn <fook.1not@gmail.com>
2023-10-10 15:23:47 +0700

    first commit

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="htmlClass">
+    <h1>{{ title }}</h1>
+  </div>
+  <InputPage/>
+  <FunctionPage/>
+</template>
+<script>
+import InputPage from "src/pages/InputPage.vue"
+import FunctionPage from "src/pages/FunctionPage.vue"
+
+export default {
+  name: 'HtmlComponent',
+  components:{
+    InputPage,
+    FunctionPage
+  },
+  data() {
+    return {
+      title: 'Sivagorn Puemjit.',
+    }
+  },
+}
+</script>
+
+<style scoped></style>
